9f2848014e1320e2e4e091ef325813971ea76a3c
Briannys <65321203+Briannys@users.noreply.github.com>
2020-11-09 16:33:16 -0500

    prueba

--- src/co/edu/ulbosque/controller/AplMain.java
+++ src/co/edu/ulbosque/controller/AplMain.java
@@ -4,5 +4,6 @@ public class AplMain {
 	public static void main(String[] args) {
 		Controller c = new Controller();
 		System.out.println("Hola que tal hola asd hola x1000");
+		System.out.println("hola");
 	}
 }

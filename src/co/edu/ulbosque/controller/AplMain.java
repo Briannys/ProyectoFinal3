ebc379ad4e8844562120916b893a5fff5683f3b6
peamolon <peamolon@192.168.2.18>
2020-11-09 16:31:01 -0500

    hola

--- src/co/edu/ulbosque/controller/AplMain.java
+++ src/co/edu/ulbosque/controller/AplMain.java
@@ -3,6 +3,6 @@ package co.edu.ulbosque.controller;
 public class AplMain {
 	public static void main(String[] args) {
 		Controller c = new Controller();
-		System.out.println("Hola que tal hola asd");
+		System.out.println("Hola que tal hola asd hola x1000");
 	}
 }

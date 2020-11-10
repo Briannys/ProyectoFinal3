39d5a92c8fa4e3b03e77ce759782ae633432f6d8
Briannys <65321203+Briannys@users.noreply.github.com>
2020-11-10 16:23:50 -0500

    ventana

--- src/co/edu/unbosque/view/VentanaPrincipal.java
+++ src/co/edu/unbosque/view/VentanaPrincipal.java
@@ -1,10 +1,23 @@
 package co.edu.unbosque.view;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+
 import javax.swing.JFrame;
 
 public class VentanaPrincipal extends JFrame {
-
+	
 	public VentanaPrincipal() {
+		setTitle("Programa Objetos de la Casa");
+		setSize(600, 500);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		getContentPane().setBackground(Color.red);
+		getContentPane().setLayout(new BorderLayout(10,10));
+		inicializarComponentes();
+		setLocationRelativeTo(null);
+		setVisible(true);
+	}
+	public void inicializarComponentes() {
 		
 	}
 }

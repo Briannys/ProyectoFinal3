f1bc8fdf6f11b2f2ed79879dfbee43a759019a04
gitForDavidL <71913304+gitForDavidL@users.noreply.github.com>
2020-11-12 16:02:42 -0500

    :)

--- src/co/edu/ulbosque/controller/Controller.java
+++ src/co/edu/ulbosque/controller/Controller.java
@@ -57,9 +57,6 @@ public class Controller implements ActionListener {
 
 			System.out.println(command);
 
-		} else if (command.equals("REGISTRARSE")) {
-
-			System.out.println(command);
 
 		} else if (command.equals("BALOTO")) {
 
